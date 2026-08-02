585edf0ff7c0ebb9a6abbb0a246c946facd9ba28
matvelmr59-cpu <matvelmr59@gmail.com>
2026-08-02 12:30:09 +0300

    Delete Фортупіано directory

--- Фортупіано/effects.py.py
+++
@@ -1,9 +0,0 @@
-import pygame
-from settings import WHITE, GREY, BLACK
-
-def draw_key_effect(screen, rect, is_pressed=False):
-    base_color = (220, 220, 220) if not is_pressed else (170, 220, 255)
-    border_color = BLACK
-
-    pygame.darw.rect(screen, base_color, rect, border_radius=8)
-    pygame.darw/rect(screen, border_color, rect, 2, border_radius=8)
--- Фортупіано/sounds.py.py
+++
@@ -1,6 +0,0 @@
-from pygame import mixer
-def load_sounds(keys):
-    sounds = {}
-    for key, filename in keys.items():
-        sounds[keys] = mixer.Sound(f"assets/sounds/{filename}")
-    return sounds
